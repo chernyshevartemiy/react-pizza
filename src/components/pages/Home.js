b4f39d738f98934656005e1d26ae01f3603bc5ec
Artem Chernyshev <arteche1@yandex.com>
2022-11-16 05:39:32 -0800

    allc: save filter url to search

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,12 +6,18 @@ import Skeleton from "../pizza-block/Skeleton";
 import Pagination from "../pagination/Pagination";
 import {SearchContext} from "../../App";
 import {useDispatch, useSelector} from "react-redux";
-import {setCategoryId, setCurrentPage} from "../../redux/slices/filterSlice";
+import {setCategoryId, setCurrentPage, setFilters} from "../../redux/slices/filterSlice";
 import axios from "axios";
+import qs from "qs"
+import {useNavigate} from "react-router-dom";
+import {list} from "../sort/Sort";
 
 function Home() {
+  const navigate = useNavigate()
   const {sort, categoryId, currentPage} = useSelector((state) => state.filterSlice)
   const dispatch = useDispatch()
+  const isSearch = React.useRef(false)
+  const isMounted = React.useRef(false)
   const onClickCategory = (id) => {
     dispatch(setCategoryId(id))
   }
@@ -23,7 +29,7 @@ function Home() {
   const [isLoading, setLoading] = React.useState(true);
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => (<Skeleton key={index}/>));
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     const search = searchValue ? `&search=${searchValue}` : '';
     const category = categoryId > 0 ? `&category=${categoryId}` : '';
     const sortBy = sort.sortProperty.replace('-', '');
@@ -39,11 +45,48 @@ function Home() {
     //   });
 
     axios.get(`https://630c81af83986f74a7c2ada9.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`).then((response) => {
+      setLoading(true)
       setItems(response.data)
       setLoading(false)
     })
     window.scrollTo(0, 0);
-  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+  }
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sort = list.find((obj) => obj.sortProperty === params.sortProperty)
+      const {currentPage, categoryId} = params;
+      // or just {...params}
+      dispatch(setFilters({
+          currentPage,
+          categoryId,
+          sort
+        }
+      ))
+      isSearch.current = true
+    }
+  }, [])
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    if (!isSearch.current) {
+      fetchPizzas()
+    }
+    isSearch.current = false;
+  }, [categoryId, sort.sortProperty, currentPage]);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage
+      })
+      navigate(`?${queryString}`)
+    }
+    isMounted.current = true
+  }, [categoryId, sort.sortProperty, currentPage])
+
   return (
     <div className="container">
       <div className="content__top">
--- src/components/sort/Sort.js
+++ src/components/sort/Sort.js
@@ -2,17 +2,18 @@ import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {setSort} from "../../redux/slices/filterSlice";
 
+export const list = [
+	{name: "популярности(DESC)", sortProperty: "rating"},
+	{name: "популярности(ASC)", sortProperty: "-rating"},
+	{name: "цене(DESC)", sortProperty: "price"},
+	{name: "цене(ASC)", sortProperty: "-price"},
+	{name: "алфавитy(DESC)", sortProperty: "title"},
+	{name: "алфавитy(ASC)", sortProperty: "-title"}]
+
 function Sort() {
 	const dispatch = useDispatch();
 	const sort = useSelector((state) => state.filterSlice.sort)
 	const [open, setOpen] = React.useState(false);
-	const list = [
-		{name: "популярности(DESC)", sortProperty: "rating"},
-		{name: "популярности(ASC)", sortProperty: "-rating"},
-		{name: "цене(DESC)", sortProperty: "price"},
-		{name: "цене(ASC)", sortProperty: "-price"},
-		{name: "алфавитy(DESC)", sortProperty: "title"},
-		{name: "алфавитy(ASC)", sortProperty: "-title"}]
 
 
 	const onClickListItem = (obj) => {
