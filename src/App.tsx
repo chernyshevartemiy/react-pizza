4afecd387cd6270729314bdd157ef6491139503f
Artem Chernyshev <artem.octopus@mail.ru>
2022-11-24 17:24:46 +0300

    app is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import "./scss/app.scss";
 import React from "react";
 import Home from "./components/pages/Home";
-import NotFound from "./components/pages/NotFound";
 import {Routes, Route} from "react-router-dom";
-import Cart from "./components/pages/Cart";
-import FullPizza from "./components/fullpizza/fullPizza";
 import MainLayout from "./layouts/MainLayout";
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */'./components/pages/Cart'))
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */'./components/fullpizza/fullPizza'))
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */'./components/pages/NotFound'))
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout/>}>
         <Route path="" element={<Home/>}/>
-        <Route path="*" element={<NotFound/>}/>
-        <Route path="/pizza/:id" element={<FullPizza/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="*" element={
+          <React.Suspense>
+            <NotFound/>
+          </React.Suspense>
+        }/>
+        <Route path="/pizza/:id" element={
+          <React.Suspense>
+            <FullPizza/>
+          </React.Suspense>
+        }/>
+        <Route path="/cart" element={
+          <React.Suspense>
+            <Cart/>
+          </React.Suspense>
+        }/>
       </Route>
     </Routes>
   );
